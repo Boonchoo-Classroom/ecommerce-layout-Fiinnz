8a2ef767b9190b2922a619a5b4fdad7b08aae784
tanaporn tantakosai <tanaporn.tant@ku.th>
2025-02-23 23:53:38 +0700

    6530202188 tanaporn tantakosai

--- app/src/main/java/scisrc/mobiledev/ecommercelayout/ui/HomeFragment.kt
+++ app/src/main/java/scisrc/mobiledev/ecommercelayout/ui/HomeFragment.kt
@@ -1,30 +1,40 @@
 package scisrc.mobiledev.ecommercelayout.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import scisrc.mobiledev.ecommercelayout.adapter.ProductAdapter
 import scisrc.mobiledev.ecommercelayout.databinding.FragmentHomeBinding
+import scisrc.mobiledev.ecommercelayout.model.ProductModel
+import scisrc.mobiledev.ecommercelayout.R
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var productAdapter: ProductAdapter
+    private val productList = mutableListOf<ProductModel>()
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // ONLY inflate layout here
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    // function to do the action in this fragment
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
